f463e6614e0fefdbba3ac45104266eb99ca540d2
vikas <vikasreddy061@gmail.com>
2025-08-05 00:44:28 -0400

    Done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
-import logo from './logo.svg';
 import './App.css';
-import PersonalDetails from './components/PersonalDetails';
-import Experience from './components/Experience';
-import Education from './components/Education';
-import Project from './components/Project';
 import ResumeBuilder from './components/ResumeBuilder';
 
 function App() {
